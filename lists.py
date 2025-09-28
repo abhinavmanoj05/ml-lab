5c276b65a23508b10ff844d44c7e64b801e5a993
Abhinav Manoj <abhinavmanoj78@gmail.com>
2025-09-29 01:08:46 +0530

    Last program

---
+++ lists.py
@@ -0,0 +1,10 @@
+list1 = [10,20,30]
+list2 = [20,40,60]
+set1 = set(list1)
+set2 = set(list2)
+union =(set1.union(set2))
+intersection = set1.intersection(set2)
+union = list(union)
+intersection  = list(intersection)
+print("Union :",union)
+print("Intersection :",intersection)
